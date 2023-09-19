845c607c540c81ec607113ba066c4ccacecef172
Isha1031 <iishasharrma@gmail.com>
2023-09-19 15:25:39 +0530

    Added search and user bar :)

--- e-waste/src/Home.js
+++ e-waste/src/Home.js
@@ -1,15 +1,20 @@
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown'
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Form, FormControl, Button } from 'react-bootstrap';
 import { Row, Col } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
 import './Home.css'
+import UserIcon from './Useric';
 function Home() {
     return (
 
+       
 
         <div className='page-container'>
 
@@ -37,34 +42,55 @@ function Home() {
                                         </NavDropdown.Item>
                                     </NavDropdown>
                                 </Nav>
+                                <Form className="d-flex">
+                                    <FormControl
+                                        type="search"
+                                        placeholder="Search here..."
+                                        className="mr-2"
+                                        aria-label="Search"
+                                    />
+                                    <Button variant="outline-success" id="searchb">
+                                        <FontAwesomeIcon icon={faSearch} /> 
+                                    </Button>
+                                </Form>
+                                <div className="d-flex align-items-center mr-50"> {/* Add a margin to this div */}
+                  {/* UserIcon component */}
+                  <UserIcon />
+                </div>
+
+
+                            
+
+
+
                             </Navbar.Collapse>
                         </Container>
                     </Navbar>
                 </div>
             </div>
             <div id='mid'>
-            
+
             </div>
             <div id='foot'>
                 <div >
                     <Row>
                         <Col md>
-                            <Row><a href="#" style={{color:'white'}}>About Us</a></Row>
-                            <Row><a href="#" style={{color:'white'}}>Our Services</a></Row>
-                            <Row><a href="#" style={{color:'white'}}>Register complains</a></Row>
+                            <Row><a href="#" style={{ color: 'white' }}>About Us</a></Row>
+                            <Row><a href="#" style={{ color: 'white' }}>Our Services</a></Row>
+                            <Row><a href="#" style={{ color: 'white' }}>Register complains</a></Row>
                         </Col>
                         <Col md>
-                            <Row><a href="#" style={{color:'white'}}>FAQ</a></Row>
-                            <Row><a href="#" style={{color:'white'}}>Order Status</a></Row>
-                            <Row><a href="#" style={{color:'white'}}>Returns</a></Row>
+                            <Row><a href="#" style={{ color: 'white' }}>FAQ</a></Row>
+                            <Row><a href="#" style={{ color: 'white' }}>Order Status</a></Row>
+                            <Row><a href="#" style={{ color: 'white' }}>Returns</a></Row>
 
                         </Col>
                         <Col md>
                             <div id='Icons'>
-                                <h4 style={{textAlign:'center'},{color:'white'}}>Follow Us</h4>
-                                <a href="#" style={{color:'white'}}><FontAwesomeIcon icon={faFacebook} /></a>
-                                <a href="#" style={{color:'white'}}><FontAwesomeIcon icon={faInstagram} id='insta' /></a>
-                                <a href="#" style={{color:'white'}}><FontAwesomeIcon icon={faTwitter} /></a>
+                                <h4 style={{ textAlign: 'center' }, { color: 'white' }}>Follow Us</h4>
+                                <a href="#" style={{ color: 'white' }}><FontAwesomeIcon icon={faFacebook} /></a>
+                                <a href="#" style={{ color: 'white' }}><FontAwesomeIcon icon={faInstagram} id='insta' /></a>
+                                <a href="#" style={{ color: 'white' }}><FontAwesomeIcon icon={faTwitter} /></a>
                             </div>
                         </Col>
                     </Row>
